93de9322d2aeb0ef96e9d426c9d003a15e1f00a5
Pistifeju <juhasz.pisti7253@gmail.com>
2023-11-07 23:08:33 +0100

    More testing

--- snippetbox/cmd/web/testutils_test.go
+++ snippetbox/cmd/web/testutils_test.go
@@ -1,17 +1,51 @@
 package main
 
 import (
+	"bytes"
+	"github.com/Pistieju/snippetbox/internal/models/mocks"
+	"github.com/alexedwards/scs/v2"
+	"github.com/go-playground/form/v4"
+	"html"
 	"io"
 	"log"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
+	"regexp"
 	"testing"
+	"time"
 )
 
+var csrfToken = regexp.MustCompile(`<input type='hidden' name='csrf_token' value='(.+)'>`)
+
+func extractCSRFToken(t *testing.T, body string) string {
+	matches := csrfToken.FindStringSubmatch(body)
+	if len(matches) < 2 {
+		t.Fatal("No CSRF token found in body")
+	}
+
+	return html.UnescapeString(matches[1])
+}
+
 func newTestApplication(t *testing.T) *application {
+	templateCache, err := newTemplateCache()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	formDecoder := form.NewDecoder()
+	sessionManager := scs.New()
+	sessionManager.Lifetime = 12 * time.Hour
+	sessionManager.Cookie.Secure = true
+
 	return &application{
-		infoLog:  log.New(io.Discard, "", 0),
-		errorLog: log.New(io.Discard, "", 0),
+		errorLog:       log.New(io.Discard, "", 0),
+		infoLog:        log.New(io.Discard, "", 0),
+		snippets:       &mocks.SnippetModel{},
+		users:          &mocks.UserModel{},
+		templateCache:  templateCache,
+		formDecoder:    formDecoder,
+		sessionManager: sessionManager,
 	}
 }
 
@@ -35,6 +69,22 @@ func (ts *testServer) get(t *testing.T, path string) (int, http.Header, string) 
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	return rs.StatusCode, rs.Header, string(body)
+}
+
+func (ts *testServer) postForm(t *testing.T, urlPath string, form url.Values) (int, http.Header, string) {
+	rs, err := ts.Client().PostForm(ts.URL+urlPath, form)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer rs.Body.Close()
+	body, err := io.ReadAll(rs.Body)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	bytes.TrimSpace(body)
 
 	return rs.StatusCode, rs.Header, string(body)
 }
--- snippetbox/internal/assert/assert.go
+++ snippetbox/internal/assert/assert.go
@@ -2,6 +2,7 @@ package assert
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 )
 
@@ -9,5 +10,12 @@ func Equal[T comparable](t *testing.T, want, got T) {
 	t.Helper()
 	if want != got {
 		panic(fmt.Sprintf("want %v; got %v", want, got))
+	}
+}
+
+func StringContains(t *testing.T, actual, expectedSubstring string) {
+	t.Helper()
+	if !strings.Contains(actual, expectedSubstring) {
+		panic(fmt.Sprintf("want %q to contain %q", actual, expectedSubstring))
 	}
 }
